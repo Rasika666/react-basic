2d67ec402a72661bdfb3900ee4e0d5dfecf972c1
Rasika Weragoda <rasika.weragodaz@gmail.com>
2019-04-19 13:56:31 +0530

    Updates

---
+++ precache-manifest.70d06f3db3375cb16ae227382714c875.js
@@ -0,0 +1,18 @@
+self.__precacheManifest = [
+  {
+    "revision": "6fd98aaa45b223576c95",
+    "url": "/react-basic/basics/static/js/runtime~main.fd00e875.js"
+  },
+  {
+    "revision": "3d7706807277312e60e2",
+    "url": "/react-basic/basics/static/js/main.687a181f.chunk.js"
+  },
+  {
+    "revision": "4fcf40feb495f39aa994",
+    "url": "/react-basic/basics/static/js/2.3fcda57f.chunk.js"
+  },
+  {
+    "revision": "6a58b0298d0c1ab45c5223b3518e25d0",
+    "url": "/react-basic/basics/index.html"
+  }
+];
